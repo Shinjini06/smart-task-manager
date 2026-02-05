02ed3ffc69de82d22681ee7dcf911a9e8452ccbe
Shinjini06 <shinjinipal.19@gmail.com>
2026-02-05 10:05:45 +0530

    Add functionality to add tasks

--- main.py
+++ main.py
@@ -1,5 +1,13 @@
+tasks = []
+
+def add_task(task):
+    tasks.append(task)
+    print(f"Task '{task}' added successfully")
+
 def main():
     print("Welcome to Smart Task Manager")
+    add_task("Sample Task")  # Test
+    print("Current Tasks:", tasks)
 
 if __name__ == "__main__":
     main()
